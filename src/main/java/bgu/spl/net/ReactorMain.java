0043dffb3a5a7626b96d3ecc4cb712c8ea42d3b7
sapir <sapir.mal13@gmail.com>
2021-01-04 17:50:41 +0200

    need to add main for the thread per client

---
+++ src/main/java/bgu/spl/net/ReactorMain.java
@@ -0,0 +1,22 @@
+package bgu.spl.net;
+
+import bgu.spl.net.srv.Reactor;
+
+import java.sql.SQLOutput;
+import java.time.LocalDateTime;
+
+
+
+public class ReactorMain {
+
+    public static void main(String[] args) {
+        Reactor server = new Reactor(
+                3,   // number of working threads
+                Integer.decode(args[1]).intValue(),  //port
+                () -> new OPMessageProtocol(),
+                () -> new OPMessageEncoderDecoder());
+        Database.getInstance().initialize(args[2]);
+        server.serve();
+    }
+
+}
